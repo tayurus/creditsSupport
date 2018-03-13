a7daaac032d88aaba1bfff4c70e043608e9d8c27
tayurus <tayurus@gmail.com>
2018-03-13 12:01:29 +0300

    fix categories block

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
-    
+
     $(".header__item-hasSubmenu").click(function(event) {
         $(event.currentTarget).next().slideToggle(200);
     });
+
+    $(".header__burger").click(function(){
+        $(".container_categories-mobile").toggle(300);
+    })
 
 })
