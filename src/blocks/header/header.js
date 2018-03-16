8d5222f6e3ba33f6dd3bd0a86f2962c61f19493d
tayurus <tayurus@gmail.com>
2018-03-16 18:07:44 +0300

    add search result page

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
 
-    $(".header__burger, .container__close").click(function(){
+    $(".header__burger, .container__close").click(function(e){
+        e.stopPropagation();
         $(".header__burger").toggleClass("open");
         $(".container_categories-mobile").slideToggle(300);
+    });
+
+    $(".container_categories-mobile").on("swipe", function(){
+        $(".header__burger").removeClass("open");
+        $(".container_categories-mobile").hide();
+    })
+
+    $(".container_categories-mobile").click(function(e){
+        e.stopPropagation();
     })
 
 })
---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("body").click( function() {
+        $(".container_categories-mobile").hide();
+        $(".header_burger").removeClass("open");
+    });
+})
